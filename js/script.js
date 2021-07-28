3e4a07cfc9cd078348c87117a55982d864589e01
Eliana-Abate <eliana.abate@outlook.it>
2021-07-28 16:10:06 +0200

    added img - edited columns

--- js/script.js
+++ js/script.js
@@ -4,5 +4,13 @@ console.log ('collegato', Vue);
 
 
 const root = new Vue({
-    
+    el: '#root',
+    data: {
+      images: [
+        './images/image1.jpg',
+        './images/image2.jpg',
+        './images/image3.jpg',
+        './images/image4.jpg',
+      ],
+    },
 });
