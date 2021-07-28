471662bc8017e28989bea3e9423ea2c6d6c5a235
Eliana-Abate <eliana.abate@outlook.it>
2021-07-28 16:44:27 +0200

    added currentIndex and function

--- js/script.js
+++ js/script.js
@@ -6,11 +6,18 @@ console.log ('collegato', Vue);
 const root = new Vue({
     el: '#root',
     data: {
-      images: [
-        './images/image1.jpg',
-        './images/image2.jpg',
-        './images/image3.jpg',
-        './images/image4.jpg',
-      ],
+        currentIndex: 0,
+        images: [
+            './images/image1.jpg',
+            './images/image2.jpg',
+            './images/image3.jpg',
+            './images/image4.jpg',
+        ],
+    },
+    methods: {
+        isCurrentImg(i) {
+            return this.currentIndex === i ? 'current' : ''; 
+        }
+
     },
 });
